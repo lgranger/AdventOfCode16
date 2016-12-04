3aac2a37aaa5e5aabecd7c8f224643cea611cfed
Lauren Granger <lr.granger21@gmail.com>
2016-12-04 09:55:30 -0800

    day3 part2

--- day3/src/com/laurengranger/TriangleCalculator.java
+++ day3/src/com/laurengranger/TriangleCalculator.java
@@ -11,30 +11,50 @@ public class TriangleCalculator {
         count = 0;
     }
 
-    public Integer countTriangles() throws Exception{
-        for (String triangleString : arrayOfTriangles) {
+    public Integer countTriangles() {
 
+        for (int i = 0; i < arrayOfTriangles.length; i = i+3) {
+
+            //set first triangle
+            String triangleString = arrayOfTriangles[i];
             triangleString = triangleString.replaceAll("\\s+", ",");
             String[] singleTriangleArray = triangleString.split(",");
-            
-            Integer triangleA = Integer.parseInt((singleTriangleArray[1]).replaceAll("\\s+", ""));
-            Integer triangleB = Integer.parseInt((singleTriangleArray[2]).replaceAll("\\s+", ""));
-            Integer triangleC = Integer.parseInt((singleTriangleArray[3]).replaceAll("\\s+", ""));
 
-            Boolean isTriangle = isTriangle(triangleA, triangleB, triangleC);
-            if (isTriangle) {
-                count = count + 1;
-            }
+            Integer triangleA1 = Integer.parseInt((singleTriangleArray[1]).replaceAll("\\s+", ""));
+            Integer triangleB1 = Integer.parseInt((singleTriangleArray[2]).replaceAll("\\s+", ""));
+            Integer triangleC1 = Integer.parseInt((singleTriangleArray[3]).replaceAll("\\s+", ""));
+
+            //set second triangle
+            triangleString = arrayOfTriangles[i+1];
+            triangleString = triangleString.replaceAll("\\s+", ",");
+            singleTriangleArray = triangleString.split(",");
+
+            Integer triangleA2 = Integer.parseInt((singleTriangleArray[1]).replaceAll("\\s+", ""));
+            Integer triangleB2 = Integer.parseInt((singleTriangleArray[2]).replaceAll("\\s+", ""));
+            Integer triangleC2 = Integer.parseInt((singleTriangleArray[3]).replaceAll("\\s+", ""));
+
+            //set third triangle
+            triangleString = arrayOfTriangles[i+2];
+            triangleString = triangleString.replaceAll("\\s+", ",");
+            singleTriangleArray = triangleString.split(",");
+
+            Integer triangleA3 = Integer.parseInt((singleTriangleArray[1]).replaceAll("\\s+", ""));
+            Integer triangleB3 = Integer.parseInt((singleTriangleArray[2]).replaceAll("\\s+", ""));
+            Integer  triangleC3 = Integer.parseInt((singleTriangleArray[3]).replaceAll("\\s+", ""));
+
+            isTriangle(triangleA1, triangleA2, triangleA3);
+            isTriangle(triangleB1, triangleB2, triangleB3);
+            isTriangle(triangleC1, triangleC2, triangleC3);
+
         }
         return count;
     }
 
-    private Boolean isTriangle(Integer triangleA, Integer triangleB, Integer triangleC){
+    private void isTriangle(Integer triangleA, Integer triangleB, Integer triangleC){
         if (((triangleA + triangleB) > triangleC)
             && ((triangleB + triangleC) > triangleA)
             && ((triangleA + triangleC) > triangleB)){
-            return true;
+            count = count + 1;
         }
-        return false;
     }
 }
